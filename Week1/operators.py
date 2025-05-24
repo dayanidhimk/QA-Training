727ee27df299ed009a184e0d75af82e5ab3f2ae4
dayanidhimk <dayanidhi9703@gmail.com>
2025-05-24 19:47:03 +0530

    Chapter2: Operators

---
+++ Week1/operators.py
@@ -0,0 +1,8 @@
+def areaOfRectangle(length, width):
+    return length*width
+
+def checkGreater(num1, num2):
+    return (num1>num2)
+
+print("Area of Rectangle (5 x 3) = ", areaOfRectangle(5,3))
+print("Is (100 > 99)?\tAnswer :", checkGreater(100,99))
