5d16682872567e63668bb51658f1aa7d2b51d411
dayanidhimk <dayanidhi9703@gmail.com>
2025-05-25 20:03:48 +0530

    Chapter 5: Lists & Tuples

---
+++ Week1/list&tuples.py
@@ -0,0 +1,6 @@
+fruits = ["apple", "orange", "watermelon", "strawberry"]
+print("Initial list :", fruits)
+fruits.append("banana")
+print("After inserting \"Banana\" :", fruits)
+fruits.pop(0)
+print("After removing 1st item :", fruits)
